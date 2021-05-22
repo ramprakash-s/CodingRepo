e648ed789a77398c29a83fa74100b4a0e86a936b
Ram <ramprakash-s@outlook.com>
2021-05-22 09:29:12 +0530

    Initial commit

---
+++ PromotionEngine/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PromotionEngine.DBRepo;
+using PromotionEngine.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ApiContext apiContext;
+
+        public ProductController(ILogger<WeatherForecastController> logger, ApiContext apiContext)
+        {
+            _logger = logger;
+            this.apiContext = apiContext;
+        }
+
+        // GET api/values  
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Product> products = apiContext.GetProducts();
+            return Ok(products);
+        }
+    }
+}
---
+++ PromotionEngine/DBRepo/ApiContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PromotionEngine.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.DBRepo
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApiContext(DbContextOptions options) : base(options)
+        {
+            LoadProducts();
+        }
+        private void LoadProducts()
+        {
+        }
+
+        /// <summary>
+        /// Retuns all product details
+        /// </summary>
+        /// <returns></returns>
+        public List<Product> GetProducts()
+        {
+            return Products.Local.ToList<Product>();
+        }
+    }
+}
